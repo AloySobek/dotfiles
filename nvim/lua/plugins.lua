2ed6bb07a365b7e53fa28df7fd267fc75ca62a49
AloySobek <supernova.wandering.star@gmail.com>
2024-12-11 22:55:35 +0300

    Asm tree sitter

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -19,7 +19,7 @@ require("lazy").setup({
         build = ":TSUpdate",
         config = function()
             require "nvim-treesitter.configs".setup({
-                ensure_installed = { "c", "lua", "query", "vim", "vimdoc", "cpp", "ocaml", "go", "python" },
+                ensure_installed = { "c", "lua", "query", "vim", "vimdoc", "cpp", "ocaml", "go", "python", "asm" },
                 highlight = {
                     enable = true,
                     additional_vim_regex_highlighting = false,
